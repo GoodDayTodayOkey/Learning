6610e4488f761c44ab22a365d6a90fd7ae6c6db5
valentin <emailaddress511@gmail.com>
2019-07-30 08:15:22 +0700

    Lesson state and Lifecycle

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,6 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import ComponentsAndProps from './Lessons/React/2.ComponentsAndProps/ComponentsAndProps';
 import MeetingJSX from './Lessons/React/1.MeetingJSX/MeetingJSX';
+import StateAndLifecycle from './Lessons/React/3.StateAndLifecycle/StateAndLifecycle';
 
-ReactDom.render(<ComponentsAndProps tool={'Raect'} />, document.getElementById('root'));
+ReactDom.render(<StateAndLifecycle />, document.getElementById('root'));
