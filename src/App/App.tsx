55015ddbed5aae2a40e7c02c6888718f503cf520
Valentin <someadress3000@gmail.com>
2019-09-06 23:18:59 +0700

    added some lessons

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
 import { block } from 'bem-cn';
 import ErrorBoundary from 'Lessons/React/10.ErrorBoundary/ErrorBoundary';
-const Parent  = React.lazy(() => import('Lessons/React/13.Portals/Parent'));
+const Parent  = React.lazy(() => import('Lessons/React/15.RenderProps/Parent'));
+const Children  = React.lazy(() => import('Lessons/React/15.RenderProps/Children'));
 
 const b = block('app');
 
 class App extends React.PureComponent {
   public render() {
     return (<div className={b()}>
-      <ErrorBoundary>
-        <React.Suspense fallback={<>Загрузка...</>}>
-          <Parent />
-        </React.Suspense>
-      </ErrorBoundary>
-      <div id={'popover'} />
+      <React.StrictMode>
+        <ErrorBoundary>
+          <React.Suspense fallback={<>Загрузка...</>}>
+            <Parent
+              render={(counter, onClick) => <Children counter={counter} onClick={onClick}/>}
+            />
+          </React.Suspense>
+        </ErrorBoundary>
+        <div id={'popover'} />
+      </React.StrictMode>
     </div>);
   }
 }
